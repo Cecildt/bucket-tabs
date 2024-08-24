f608cbe4706dff52aa3bf92823e8092c9f74f7bc
cecil <cecildt@gmail.com>
2024-08-25 00:26:04 +0200

    feat: Add bucket tabs functionality
    This commit adds the functionality for bucket tabs, allowing users to organize their tabs into different buckets. It includes the necessary changes to the index page and the sleep page, as well as updates to the background script to handle tab activation and updates.

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -19,3 +19,27 @@ chrome.runtime.onSuspendCanceled.addListener(function () {
     console.log('Extension resumed');
     setupContextMenu();
 });
+
+chrome.runtime.onUpdateAvailable.addListener(function () {
+    console.log('Extension updated');
+    setupContextMenu();
+});
+
+chrome.runtime.onConnect.addListener(function (port) {
+    console.log('Connected to port: ', port);
+});
+
+chrome.runtime.onRestartRequired.addListener(function (reason) {
+    console.log('Restart required: ', reason);
+});
+
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    console.log('Tab activated: ', activeInfo);
+    setupContextMenu();
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    console.log('Tab updated: ', tabId, changeInfo, tab);
+    setupContextMenu();
+});

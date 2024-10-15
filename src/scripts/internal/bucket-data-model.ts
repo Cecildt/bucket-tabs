f44abb8e2d626cc6ec7223070558c9b113221aef
cecil <cecildt@gmail.com>
2024-10-15 22:09:14 +0200

    Refactor storage logic and update class names in bucket-data-model.ts

--- src/scripts/internal/bucket-data-model.ts
+++ src/scripts/internal/bucket-data-model.ts
@@ -1,119 +1,168 @@
-import { ulid } from 'ulid'
-import { setBucketsStorage, getBucketsStorage, getArchivedStorage, setArchivedStorage } from './storage'
+import { ulid } from 'ulid';
+import {
+  setBucketsStorage,
+  getBucketsStorage,
+  getArchivedStorage,
+  setArchivedStorage,
+} from './storage';
 
 export class BucketListDataModel {
-    private Buckets: Array<BucketDataModel> = [];
-    private Archived: Array<BucketDataModel> = [];
+  private Buckets: Array<BucketDataModel> = [];
+  private Archived: Array<BucketDataModel> = [];
 
-    constructor() {               
+  constructor() {}
+
+  async initDataSet(): Promise<void> {
+    let buckets = await getBucketsStorage();
+    let archived = await getArchivedStorage();
+
+    if (buckets.length > 0) {
+      this.Buckets = buckets;
+    } else {
+      this.Buckets.push(new BucketDataModel(undefined, 'Default Bucket', 1));
+      setBucketsStorage(this.Buckets);
     }
 
-    async initDataSet(): Promise<void> {
-
-        let buckets = await getBucketsStorage();
-        let archived = await getArchivedStorage();
-
-        if (buckets.length > 0) {
-            this.Buckets = buckets;
-        } else {
-            this.Buckets.push(new BucketDataModel('Default Bucket', 1));
-            setBucketsStorage(this.Buckets);
-        }
-
-        if (archived.length > 0) {
-            this.Archived = archived;
-        } else {
-            this.Archived.push(new BucketDataModel('Archived Bucket', 1));
-            setArchivedStorage(this.Archived);
-        }
+    if (archived.length > 0) {
+      this.Archived = archived;
+    } else {
+      this.Archived.push(new BucketDataModel(undefined, 'Archived Bucket', 1));
+      setArchivedStorage(this.Archived);
     }
+  }
 
-    initDemoDataSet(): void {
-        this.Buckets.push(new BucketDataModel('Default Bucket', 1));
-        this.Buckets.push(new BucketDataModel('Demo Bucket', 2));
-        this.Archived.push(new BucketDataModel('Archived Bucket', 1));
+  initDemoDataSet(): void {
+    this.Buckets.push(new BucketDataModel(undefined, 'Default Bucket', 1));
+    this.Buckets.push(new BucketDataModel(undefined, 'Demo Bucket', 2));
+    this.Archived.push(new BucketDataModel(undefined, 'Archived Bucket', 1));
 
-        setBucketsStorage(this.Buckets);
-        setArchivedStorage(this.Archived);
+    setBucketsStorage(this.Buckets);
+    setArchivedStorage(this.Archived);
+  }
+
+  addBucket(bucket: BucketDataModel): void {
+    bucket.setOrder(this.Buckets.length + 1);
+    this.Buckets.push(bucket);
+    setBucketsStorage(this.Buckets);
+  }
+
+  removeBucket(bucket: BucketDataModel): void {
+    let index = this.Buckets.indexOf(bucket);
+    if (index > -1) {
+      this.Buckets.splice(index, 1);
     }
+    setBucketsStorage(this.Buckets);
+  }
 
-    addBucket(bucket: BucketDataModel): void {
-        bucket.setOrder(this.Buckets.length+1);
-        this.Buckets.push(bucket);
-        setBucketsStorage(this.Buckets);
-    }
-
-    removeBucket(bucket: BucketDataModel): void {
-        let index = this.Buckets.indexOf(bucket);
-        if (index > -1) {
-            this.Buckets.splice(index, 1);
-        }
-        setBucketsStorage(this.Buckets);
-    }
-
-    getBuckets(): Array<BucketDataModel> {
-        return this.Buckets;
-    }
+  getBuckets(): Array<BucketDataModel> {
+    return this.Buckets;
+  }
 }
 
-
 export class BucketDataModel {
-    private BucketID: String = ulid();
-    private BucketName: String = '';
-    private Order: Number = -1;
-    private BucketTabs: Array<TabDataModel> = [];    
-    
-    constructor(name: String, order: Number) {
-        this.BucketName = name;        
-        this.Order = order;
-        console.log(this.BucketID);
-    }
+  private BucketID: String = ulid();
+  private BucketName: String = '';
+  private Order: Number = -1;
+  private BucketTabs: Array<TabDataModel> = [];
 
-    addTab(tab: TabDataModel): void {
-        tab.setOrder(this.BucketTabs.length + 1);
-        this.BucketTabs.push(tab);
-    }
+  constructor(id: String = ulid(), name: String, order: Number) {
+    this.BucketID = id;
+    this.BucketName = name;
+    this.Order = order;
+    console.log(this.BucketID);
+  }
 
-    removeTab(tab: TabDataModel): void {
-        let index = this.BucketTabs.indexOf(tab);
-        if (index > -1) {
-            this.BucketTabs.splice(index, 1);
-        }
-    }
+  addTab(tab: TabDataModel): void {
+    tab.setOrder(this.BucketTabs.length + 1);
+    this.BucketTabs.push(tab);
+  }
 
-    getTabs(): Array<TabDataModel> {
-        return this.BucketTabs;
+  removeTab(tab: TabDataModel): void {
+    let index = this.BucketTabs.indexOf(tab);
+    if (index > -1) {
+      this.BucketTabs.splice(index, 1);
     }
+  }
 
-    setOrder(order: Number): void {
-        this.Order = order;
-    }
+  getTabs(): Array<TabDataModel> {
+    return this.BucketTabs;
+  }
 
-    getBucketID(): String {
-        return this.BucketID;
-    }
+  getOrder(): Number {
+    return this.Order;
+  }
 
-    getBucketName(): String {
-        return this.BucketName
-    }
+  setOrder(order: Number): void {
+    this.Order = order;
+  }
+
+  getBucketID(): String {
+    return this.BucketID;
+  }
+
+  getBucketName(): String {
+    return this.BucketName;
+  }
 }
 
 export class TabDataModel {
-    private TabID: Number = -1;
-    private TabName: String = '';    
-    private TabURL: String = '';
-    private TabBucketID: Number = -1;
-    private Order: Number = -1;
-    
-    constructor(id: Number, name: String, url: String, bucketID: Number, order: Number) {
-        this.TabID = id;
-        this.TabName = name;
-        this.TabURL = url;
-        this.TabBucketID = bucketID;
-        this.Order = order;
-    }
+  private TabID: Number = -1;
+  private TabName: String = '';
+  private TabURL: String = '';
+  private TabBucketID: Number = -1;
+  private Order: Number = -1;
 
-    setOrder(order: Number): void {
-        this.Order = order;
-    }
+  constructor(
+    id: Number,
+    name: String,
+    url: String,
+    bucketID: Number,
+    order: Number
+  ) {
+    this.TabID = id;
+    this.TabName = name;
+    this.TabURL = url;
+    this.TabBucketID = bucketID;
+    this.Order = order;
+  }
+
+  setOrder(order: Number): void {
+    this.Order = order;
+  }
+
+  getOrder(): Number {
+    return this.Order;
+  }
+
+  getTabID(): Number {
+    return this.TabID;
+  }
+
+  getTabName(): String {
+    return this.TabName;
+  }
+
+  getTabURL(): String {
+    return this.TabURL;
+  }
+
+  getTabBucketID(): Number {
+    return this.TabBucketID;
+  }
+
+  setTabBucketID(id: Number): void {
+    this.TabBucketID = id;
+  }
+
+  setTabName(name: String): void {
+    this.TabName = name;
+  }
+
+  setTabURL(url: String): void {
+    this.TabURL = url;
+  }
+
+  setTabID(id: Number): void {
+    this.TabID = id;
+  }
 }
--- src/scripts/internal/storage.ts
+++ src/scripts/internal/storage.ts
@@ -1,11 +1,20 @@
 // Description: Logic for storing and retrieving data from local storage
 
-import { BucketDataModel } from "./bucket-data-model";
+import { BucketDataModel, TabDataModel } from './bucket-data-model';
 
 interface BucketStorageDataModel {
-    BucketID: String;
-    BucketName: String;
-    Order: Number;
+  BucketID: String;
+  BucketName: String;
+  Order: Number;
+  BucketTabs: Array<TabStorageDataModel>;
+}
+
+interface TabStorageDataModel {
+  TabID: Number;
+  TabName: String;
+  TabURL: String;
+  TabBucketID: Number;
+  Order: Number;
 }
 
 export function initStorage(): void {
@@ -24,26 +33,123 @@ export function getBucketsStorage(): Promise<Array<BucketDataModel>> {
   let storedBuckets: Array<BucketDataModel> = [];
 
   getStorage('buckets').then((buckets: Array<BucketStorageDataModel>) => {
-      if (buckets.length === 0) {
-        buckets.forEach((bucket: BucketStorageDataModel) => {
-        storedBuckets.push(new BucketDataModel(bucket.BucketName, bucket.Order));
+    if (buckets.length === 0) {
+      buckets.forEach((bucket: BucketStorageDataModel) => {
+        let newBucket = new BucketDataModel(
+          bucket.BucketID,
+          bucket.BucketName,
+          bucket.Order
+        );
+
+        bucket.BucketTabs.forEach((tab: TabStorageDataModel) => {
+          let tabData = new TabDataModel(
+            tab.TabID,
+            tab.TabName,
+            tab.TabURL,
+            tab.TabBucketID,
+            tab.Order
+          );
+          newBucket.addTab(tabData);
         });
-      }
+
+        storedBuckets.push(newBucket);
+      });
+    }
   });
 
   return Promise.resolve(storedBuckets);
 }
 
 export function setBucketsStorage(buckets: Array<BucketDataModel>): void {
-  setStorage('buckets', buckets);
+  let storageBuckets: Array<BucketStorageDataModel> = [];
+
+  buckets.forEach((bucket: BucketDataModel) => {
+    let storedTabs: Array<TabStorageDataModel> = [];
+
+    bucket.getTabs().forEach((tab: TabDataModel) => {
+      let storedTab: TabStorageDataModel = {
+        TabID: tab.getTabID(),
+        TabName: tab.getTabName(),
+        TabURL: tab.getTabURL(),
+        TabBucketID: tab.getTabBucketID(),
+        Order: tab.getOrder(),
+      };
+
+      storedTabs.push(storedTab);
+    });
+    
+    let storageBucket: BucketStorageDataModel = {
+      BucketID: bucket.getBucketID(),
+      BucketName: bucket.getBucketName(),
+      Order: bucket.getOrder(),
+      BucketTabs: storedTabs,
+    };
+
+    storageBuckets.push(storageBucket);
+  });
+
+  setStorage('buckets', storageBuckets);
 }
 
 export function getArchivedStorage(): Promise<Array<BucketDataModel>> {
-  return getStorage('archived');
+  let storedBuckets: Array<BucketDataModel> = [];
+
+  getStorage('archived').then((buckets: Array<BucketStorageDataModel>) => {
+    if (buckets.length === 0) {
+      buckets.forEach((bucket: BucketStorageDataModel) => {
+        let newBucket = new BucketDataModel(
+          bucket.BucketID,
+          bucket.BucketName,
+          bucket.Order
+        );
+
+        bucket.BucketTabs.forEach((tab: TabStorageDataModel) => {
+          let tabData = new TabDataModel(
+            tab.TabID,
+            tab.TabName,
+            tab.TabURL,
+            tab.TabBucketID,
+            tab.Order
+          );
+          newBucket.addTab(tabData);
+        });
+        storedBuckets.push(newBucket);
+      });
+    }
+  });
+
+  return Promise.resolve(storedBuckets);
 }
 
 export function setArchivedStorage(archived: Array<BucketDataModel>): void {
-  setStorage('archived', archived);
+  let storageBuckets: Array<BucketStorageDataModel> = [];
+
+  archived.forEach((bucket: BucketDataModel) => {
+    let storageTabs: Array<TabStorageDataModel> = [];
+
+    bucket.getTabs().forEach((tab: TabDataModel) => {
+      let storageTab: TabStorageDataModel = {
+        TabID: tab.getTabID(),
+        TabName: tab.getTabName(),
+        TabURL: tab.getTabURL(),
+        TabBucketID: tab.getTabBucketID(),
+        Order: tab.getOrder(),
+      };
+
+      storageTabs.push(storageTab);
+    });
+    
+    let storageBucket: BucketStorageDataModel = {
+      BucketID: bucket.getBucketID(),
+      BucketName: bucket.getBucketName(),
+      Order: bucket.getOrder(),
+      BucketTabs: storageTabs,
+    };
+
+    storageBuckets.push(storageBucket);
+  });
+
+  setStorage('archived', storageBuckets);
 }
 
 export function getStorage(key: string): Promise<any> {
